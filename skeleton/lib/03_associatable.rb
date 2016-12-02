c247450faff6dc1cd854444c8596e4e33dccdd4d
Preston Phelan <preston.phelan@gmail.com>
2016-12-02 13:29:17 -0800

    Added assoc_options method for storing options for belongs_to

--- skeleton/lib/03_associatable.rb
+++ skeleton/lib/03_associatable.rb
@@ -38,13 +38,14 @@ end
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    options = BelongsToOptions.new(name, options)
+    assoc_options
+    @assoc_options[name] = BelongsToOptions.new(name, options)
 
     define_method(name) do
-      target_class = options.model_class
-      foreign_key_value = send(options.foreign_key)
+      target_class = self.class.assoc_options.model_class
+      foreign_key_value = send(self.class.assoc_options.foreign_key)
 
-      target_class.where(options.primary_key.to_sym => foreign_key_value).first
+      target_class.where(self.class.assoc_options.primary_key.to_sym => foreign_key_value).first
     end
   end
 
@@ -61,6 +62,7 @@ module Associatable
 
   def assoc_options
     # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
   end
 end
 
