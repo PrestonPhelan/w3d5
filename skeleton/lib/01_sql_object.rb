1a353e8523ebfa902a8c0c59bf29077a0cfaf131
Preston Phelan <preston.phelan@gmail.com>
2016-12-02 11:38:34 -0800

    table_name, columns, attributes, finalize, initialize, all, find and insert functionality added

--- skeleton/lib/01_sql_object.rb
+++ skeleton/lib/01_sql_object.rb
@@ -5,46 +5,97 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    return @columns if @columns
+    data = DBConnection.execute2(<<-SQL)
+      SELECT *
+      FROM #{table_name}
+      LIMIT 1
+    SQL
+
+    @columns = data.first.map(&:to_sym)
   end
 
   def self.finalize!
+    columns.each do |column|
+      define_method(column) { attributes[column] }
+      define_method("#{column}=") do |value|
+        attributes[column] = value
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name || self.name.tableize
   end
 
   def self.all
-    # ...
+    results = DBConnection.execute(<<-SQL)
+      SELECT *
+      FROM #{table_name}
+    SQL
+
+    parse_all(results)
   end
 
   def self.parse_all(results)
-    # ...
+    objects = []
+    results.each do |result|
+      objects << self.new(result)
+    end
+
+    objects
   end
 
   def self.find(id)
-    # ...
+    result = DBConnection.execute(<<-SQL, id)
+      SELECT *
+      FROM #{table_name}
+      WHERE id = ?
+    SQL
+
+    parse_all(result).first
   end
 
   def initialize(params = {})
-    # ...
+    self.class.finalize!
+
+    params.each do |attr_name, value|
+      column_name = attr_name.to_sym
+      unless self.class.columns.include?(column_name)
+        raise "unknown attribute '#{attr_name}'"
+      end
+
+      send("#{column_name}=", value)
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
-    # ...
+
+    self.class.columns.map do |column|
+      send(column)
+    end
   end
 
   def insert
-    # ...
+    col_names = self.class.columns.join(", ")
+    question_marks = (["?"] * self.class.columns.length).join(", ")
+
+    DBConnection.execute(<<-SQL, attribute_values)
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks})
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
   end
 
   def update
