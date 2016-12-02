845395971eaf8c58e509356b1281fbe53a4a71fb
Preston Phelan <preston.phelan@gmail.com>
2016-12-02 14:13:29 -0800

    Completed Phase 4, added has_one_through functionality (using 2 queries)

--- skeleton/lib/04_associatable2.rb
+++ skeleton/lib/04_associatable2.rb
@@ -5,6 +5,28 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    through_options = assoc_options[through_name]
+    source_options = through_options.model_class.assoc_options[source_name]
+
+    define_method(name) do
+      through_class = through_options.model_class
+      through_fk_value = send(through_options.foreign_key)
+      through_pk_column = through_options.primary_key.to_sym
+
+      through_result = through_class
+        .where(through_pk_column => through_fk_value).first
+
+
+      source_class = source_options.model_class
+      source_fk_value = through_result.send(source_options.foreign_key)
+      source_pk_column = source_options.primary_key.to_sym
+
+      source_class.where(source_pk_column => source_fk_value).first
+
+
+      #self.send(through_name).send(source_name)
+    end
+
+
   end
 end
