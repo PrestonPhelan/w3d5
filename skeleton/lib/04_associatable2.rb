b0386d09a0a0c4b8b0749a912194fa3e27afbe03
Preston Phelan <preston.phelan@gmail.com>
2016-12-02 14:27:28 -0800

    Updated has_one_through to run single SQL query

--- skeleton/lib/04_associatable2.rb
+++ skeleton/lib/04_associatable2.rb
@@ -9,23 +9,23 @@ module Associatable
 
     define_method(name) do
       source_options = through_options.model_class.assoc_options[source_name]
-      
-      through_class = through_options.model_class
-      through_fk_value = send(through_options.foreign_key)
-      through_pk_column = through_options.primary_key.to_sym
 
-      through_result = through_class
-        .where(through_pk_column => through_fk_value).first
+      source_table = source_options.model_class.table_name
+      through_table = through_options.model_class.table_name
 
+      result = DBConnection.execute(<<-SQL)
+      SELECT
+        #{source_table}.*
+      FROM
+        #{source_table}
+      JOIN
+        #{through_table} ON #{source_table}.#{source_options.primary_key} = #{through_table}.#{source_options.foreign_key}
+      WHERE
+        #{through_table}.#{through_options.primary_key} = #{send(through_options.foreign_key)}
+      SQL
 
-      source_class = source_options.model_class
-      source_fk_value = through_result.send(source_options.foreign_key)
-      source_pk_column = source_options.primary_key.to_sym
-
-      source_class.where(source_pk_column => source_fk_value).first
-
-
-      #self.send(through_name).send(source_name)
+      House.new(result.first)
+      #self.send(through_name).send(source_name), one-liner if you want it :-)
     end
 
 
